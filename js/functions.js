bf33daf223b351d35bf3c700100895d2dbbf78ce
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-08-27 19:28:51 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function($) {
 	$('.mobile-nav').click(function() {
 		$('.top-menu-nav').slideToggle();
+	});
+
+	$('.top-nav-menu-open').click(function() {
+		$(this).next('nav').slideToggle();
 	});;
 });
