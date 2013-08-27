eedf1b2b84af4b9629305a58970a71db2d34dfca
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-08-27 22:47:14 +0400

    update mobile nav script

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,15 @@
 jQuery(document).ready(function($) {
+
+	if ($('.mobile-nav').is(':visible')) {
+
+
 	$('.mobile-nav').click(function() {
 		$('.top-menu-nav').slideToggle();
-		$(this).toggleClass('mobile-nav-open');
+		$(this).toggleClass('mobile-nav-open-active');
 	});
 
 	$('.top-nav-menu-open').click(function() {
 		$(this).toggleClass('mobile-active-open').next('nav').slideToggle();
 	});;
+} 
 });
